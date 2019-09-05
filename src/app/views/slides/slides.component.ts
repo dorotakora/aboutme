3a3a275b4ee72c17cde47ac5c45a57ce6230f181
dorotakora <dorota.kora@o2.pl>
2019-09-05 21:28:43 +0200

    cahnge

--- src/app/views/slides/slides.component.ts
+++ src/app/views/slides/slides.component.ts
@@ -7,6 +7,8 @@ import { Router } from "@angular/router";
   templateUrl: "./slides.component.html"
 })
 export class SlidesComponent {
+  public activeslide: number = 0;
+
   constructor(private _CvService: CvService, private _Router: Router) {
     console.log("Slides component");
     // this._CvService.test();
@@ -15,5 +17,21 @@ export class SlidesComponent {
   gotoLandingPage() {
     console.log("gotoLandingPage()");
     this._Router.navigate([""]);
+  }
+
+  navigate(direction: number) {
+    console.log("navigate()");
+
+    if (this.activeslide >= 0 && this.activeslide <= 3) {
+      this.activeslide += direction;
+
+      // document
+      //   .querySelectorAll("div[class*='slide-']")
+      //   .forEach(slide => slide.classList.remove("active"));
+
+      // document
+      //   .querySelector(`.slide-${this.activeslide}`)
+      //   .classList.add("active");
+    }
   }
 }
