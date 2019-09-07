038a9ae08a111776d0cc63d9da730e2ed594dbf6
dorotakora <dorota.kora@o2.pl>
2019-09-07 17:32:10 +0200

    slider

--- src/app/views/slides/slides.component.ts
+++ src/app/views/slides/slides.component.ts
@@ -24,32 +24,22 @@ export class SlidesComponent {
       this.activeslide += slide;
 
       Array.from(document.querySelectorAll(".slides")).forEach(slide =>
-        slide.classList.remove("slide-in-right")
-      );
-      Array.from(document.querySelectorAll(".slides")).forEach(slide =>
-        slide.classList.remove("slide-out-right")
-      );
-      Array.from(document.querySelectorAll(".slides")).forEach(slide =>
-        slide.classList.remove("slide-in-left")
-      );
-      Array.from(document.querySelectorAll(".slides")).forEach(slide =>
-        slide.classList.remove("slide-out-left")
+        slide.classList.remove(
+          "slide-in-right",
+          "slide-out-right",
+          "slide-in-left",
+          "slide-out-left"
+        )
       );
 
       if (this.direction === "next") {
         Array.from(document.querySelectorAll(".slides"))[
           this.activeslide
         ].classList.add("slide-in-right");
-        // Array.from(document.querySelectorAll(".slides"))[
-        //   this.activeslide - 1
-        // ].classList.add("slide-out-left");
       } else if (this.direction === "previous") {
         Array.from(document.querySelectorAll(".slides"))[
           this.activeslide
         ].classList.add("slide-in-left");
-        // Array.from(document.querySelectorAll(".slides"))[
-        //   this.activeslide + 1
-        // ].classList.add("slide-out-right");
       }
     }
   }
